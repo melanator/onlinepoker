0164e2a6975d7cadbe740ec8f858b4403c7f96eb
Anton Shevchuk <ashevchuk22@gmail.com>
2022-07-24 20:38:56 +1100

    First commit

---
+++ src/somegame.cpp
@@ -0,0 +1,12 @@
+﻿// somegame.cpp: определяет точку входа для приложения.
+//
+
+#include "../include/somegame.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
