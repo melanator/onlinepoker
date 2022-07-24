3216f611fd5ffeb88a464f5afcd6d38e290dd71d
Anton Shevchuk <ashevchuk22@gmail.com>
2022-07-24 20:55:28 +1100

    Server client first commit

---
+++ src/client/client.cpp
@@ -0,0 +1,8 @@
+#include "../../include/somegame.h"
+
+using namespace std;
+
+int main() {
+	cout << "Client" << endl;
+	return 0;
+}
---
+++ src/server/server.cpp
@@ -0,0 +1,8 @@
+#include "../../include/somegame.h"
+
+using namespace std;
+
+int main() {
+	cout << "Server" << endl;
+	return 0;
+}
