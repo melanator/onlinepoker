a92b306406a01e28e6b55ac502340950b6716d24
Anton <ashevchuk22@gmail.com>
2022-07-29 16:51:57 +1100

    Eternal serer on Windows

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -6,6 +6,79 @@
 #include "../include/server.h"
 
 using namespace std;
+DWORD WINAPI ThreadFun(LPVOID lpParam){
+	cout << "Thread Running" << endl;
+	return 0;
+}
+
+struct fork_struct{
+	sockaddr_in client;
+	SOCKET clientSocket;
+};
+
+int socket_handling(SOCKET clientSocket, sockaddr_in *client){	
+	char host[NI_MAXHOST]; 		// Cleint's remote name
+	char service[NI_MAXHOST];	// Port the client is connect on
+
+	memset(host, 0, NI_MAXHOST);
+	memset(service, 0, NI_MAXHOST);
+
+	if(getnameinfo((sockaddr*)client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0){
+		cout << host << " connected on port " << service << endl;
+	} else {
+		inet_ntop(AF_INET, &(*client).sin_addr, host, NI_MAXHOST);
+		cout << host << " connected on port " << ntohs((*client).sin_port) << endl;
+	}
+
+	char buf[4096];
+	while (true){
+		memset(buf, 0, sizeof(buf));
+
+		// Wait for client to send data
+		int bytesRecieved = recv(clientSocket, buf, size(buf), 0);
+		if (bytesRecieved == SOCKET_ERROR) {
+			cout << "Error in revc()" << endl;
+			break;
+		}
+		if (bytesRecieved == 0){
+			cout << "Client  " << host << ":" << ntohs((*client).sin_port) <<  " disconneted" << endl;
+			break;
+		}
+
+		// Echo message back to client
+		cout << "From " << host << " ";
+		for(int i = 0; i < sizeof(buf); i++){
+			cout << buf[i];
+		}
+		cout << endl;
+		send(clientSocket, buf, bytesRecieved + 1, 0);
+	}
+
+	return 0;
+}
+
+DWORD windows_fork(LPVOID lpParam){
+	fork_struct *params = static_cast<fork_struct*>(lpParam);
+
+	sockaddr_in *client = &(*params).client;
+	int clientSize = sizeof(client);
+	SOCKET clientSocket = (*params).clientSocket;
+
+	socket_handling(clientSocket, client);
+	
+	// Close the socket
+	closesocket(clientSocket);
+	return 0;
+}
+
+void linux_fork(sockaddr_in* client, SOCKET clientSocket){
+
+	socket_handling(clientSocket, client);
+
+	#ifndef WIN_BUILD
+	close(clientSocket);
+	#endif
+}
 
 int main() {
 	#ifdef WIN_BUILD
@@ -18,9 +91,6 @@ int main() {
 		cout << "Cant initialize winsock!" << endl;
 		return 1;
 	}
-
-	#else
-	// Initialize sock on Linux
 	#endif // WIN_BUILD
 
 	// Create a socket
@@ -38,79 +108,48 @@ int main() {
 
 	bind(listening, (sockaddr*)&hint, sizeof(hint));
 
-	listen(listening, SOMAXCONN);
+	listen(listening, 2048);
 	// Wait for a connection
 	sockaddr_in client;
 	int clientSize = sizeof(client);
 
-	SOCKET clientSocket = accept(
-		listening, 
-		(sockaddr*)&client, 
-			#ifndef WIN_BIULD // Unix build size signature in socklen_t*, windows int*
-			(socklen_t*)
-			#endif	
-		&clientSize);
-	if (clientSocket == INVALID_SOCKET){
-		cout << "Failed client Socket";
-		return 3;
-	}
+	#ifdef WIN_BUILD
+	HANDLE socketsThread[2048];
+	fork_struct structsThread[2048];
+	int current_socket = 0;
 
-	char host[NI_MAXHOST]; 		// Cleint's remote name
-	char service[NI_MAXHOST];	// Port the client is connect on
-
-	memset(host, 0, NI_MAXHOST);
-	memset(service, 0, NI_MAXHOST);
-
-	if(getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0){
-		cout << host << " connnnected on port " << service << endl;
-	} else {
-		inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
-		cout << host << " connected on port " << ntohs(client.sin_port) << endl;
-	}
-
-	// Close listening socket
-#ifdef WIN_BUILD
-	closesocket(listening);
-#else
-	close(listening);
-#endif // WIN_BUILD
-
-
-	// While loop: accept and echo back message to client
-	char buf[4096];
-
-	while (true){
-		memset(buf, 0, 4096);
-
-		// Wait for client to send data
-		int bytesRecieved = recv(clientSocket, buf, 4096, 0);
-		if (bytesRecieved == SOCKET_ERROR) {
-			cout << "Error in revc()" << endl;
-			break;
-		}
-		if (bytesRecieved == 0){
-			cout << "Client disconneted" << endl;
-			break;
+	while (true)
+	{
+		SOCKET clientSocket = accept(
+			listening, 
+			(sockaddr*)&client, 
+				#ifndef WIN_BIULD // Unix build size signature in socklen_t*, windows int*
+				(socklen_t*)
+				#endif	
+			&clientSize);
+		if (clientSocket == INVALID_SOCKET){
+			cout << "Failed client Socket";
+			return 3;
 		}
 
-		// Echo message back to client
-		cout << "From " << host << " ";
-		for(int i = 0; i < 4096; i++){
-			cout << buf[i];
-		}
-		cout << endl;
-		send(clientSocket, buf, bytesRecieved + 1, 0);
+		(structsThread[current_socket]).client = client;
+		(structsThread[current_socket]).clientSocket = clientSocket;
+
+		socketsThread[current_socket]  = CreateThread(
+				NULL,
+				0,
+				windows_fork,
+				(LPVOID) &(structsThread[current_socket]),
+				0,
+				NULL
+			);
+
 	}
 	
-	// Close the socket
-#ifdef WIN_BUILD
-	closesocket(clientSocket);
-	// Clean WSocket
 	WSACleanup();
-#else
-	close(clientSocket);
-#endif // WIN_BUILD
-
+	#else
+		fork()
+	#endif // WIN_BUILD
 
 	return 0;
 }
