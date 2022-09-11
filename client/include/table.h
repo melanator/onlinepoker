939dc0f6ea01bfa94b3f6663a14da2c9cfb058d4
Anton Shevchuk <ashevchuk22@gmail.com>
2022-09-12 01:14:37 +1100

    Table interface

--- client/include/table.h
+++ client/include/table.h
@@ -1,33 +1,94 @@
 // Data structure imitating table
 // Realization - linked list, but last node links to first node
+#include <iostream>
 
 template <typename Type>
 class Table {
-
+public:
 	struct Node {
 		Type* val;
-		Node* next;
+		Node* next = nullptr;
 	};
 
-public:
 	Table(){};
-	Table(Type& val) {
-		first = { &val, nullptr };
-		first.next = &first;
+	Table(Type val) {
+		first = new Node({ new Type(val), nullptr });
+		first->next = first;
 		size = 1;
 	};
-	void push_front(Type node) {	}
+	Table(Type* ptr) {
+		first = new Node({ ptr, nullptr });
+		first->next = first;
+		size = 1;
+	}
+
+	~Table() {
+		Node* first_elem = first;
+		do {
+			Node* tmp = first->next;
+			delete first->val;
+			delete first;
+			first = tmp;
+		} while (first_elem != first->next);
+	}
+
+	void push_front(Type val) {
+		Node* tmp = first->next;
+		first->next = new Node({ new Type(val), nullptr });
+		first->next->next = tmp;
+		size++;
+	}
+	void push_front(Type* ptr){	
+		Node* tmp = first->next;
+		first->next = new Node({ ptr, nullptr });
+		first->next->next = tmp;
+		size++;
+	}
+
 //	void push_back(Type node)
 //  void push(Node* node, size_t pos);
 //	Node pop_front();
 //	Node pop_back();
-//	Node pop();
-	void shift() { first = *first.next; }
+//	Node pop(Type* elem);
+
+	void shift() {
+		first = first->next;
+	}
+	const Node* GetFirst() const {
+		return first;
+	}
 	size_t get_size();
+	
+	Node& operator[] (int index) {
+		Node* result = first;
+		for (int i = 0; i < index; i++) {
+			result = result->next;
+		}
+		return *result;
+	}
 
-//	int& operator[] (int index);
-
-private:
-	Node first;
+	Node* first;
 	size_t size;
 };
+
+template <typename Type>
+std::ostream& operator<< (std::ostream& os, Table<Type>& table) {
+	if (table.size > 1) {
+		const Table<Type>::Node* first_elem = table.first;
+		do {
+			std::cout << *(table.first->val) << " ";
+			table.first = table.first->next;
+		} while (first_elem != table.first);	
+	}
+	else if (table.size == 1) {
+		os << *(table.first->val);
+	}
+	
+	return os;
+}
+
+template <typename Type>
+std::ostream& operator<< (std::ostream& os, typename Table<Type>::Node& node) {
+	os << *(node.val);
+	return os;
+}
--- client/src/table.cpp
+++ client/src/table.cpp
@@ -1,9 +1,15 @@
 #include "../include/table.h"
+using namespace std;
 
 int main() {
-	int a = 5;
-	Table<int> table(a);
-	int b = 2;
-	table.push_front(b);
-	table.shift();
+
+	{
+		int a = 5;
+		Table<int> table(3);
+		table.push_front(4);
+		table.push_front(5);
+		std::cout << table << std::endl;
+		std::cout << *table.first;
+		
+	}
 }
